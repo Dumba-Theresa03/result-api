3346063c657c338a290869d770d8982c535a9106
Dumba Theresa <101268896+Dumba-Theresa03@users.noreply.github.com>
2022-04-06 15:47:59 +0100

    first commit

---
+++ results.js
@@ -0,0 +1,9 @@
+const results=[
+{name:"Thessy Dumba", class:"form one", marks:"8"},
+{name:"Windy Bayuo", class:"form one", marks:"7"},
+{name:"Flex Dassah", class:"form one", marks:"6"},
+{name:"Florida Natie", class:"form one", marks:"5"},
+{name:"James Dari", class:"form one", marks:"4"},
+
+]
+module.exports=results
